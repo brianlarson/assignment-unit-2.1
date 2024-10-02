a94b0813ace15b1ee0f3359345202dd3533cb070
Brian Larson <brian@brianlarson.com>
2024-10-01 21:12:52 -0500

    Create undefined result var and conditional checking if adventurous var is true

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -53,6 +53,13 @@ let allowedPets = 2;
 //      if adventurous is true, set `result` to be "Adventures are great!",
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
+let result;
+if (adventurous) {
+    result = `Adventures are great!`;
+} else {
+    result = `How about we stay home?`;
+}
+console.log(result);
 
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
 //      Create a compound conditional:
