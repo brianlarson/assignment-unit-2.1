2ce23189018c9e93afa51ed2f7cd1c7a417fcad4
Brian Larson <brian@brianlarson.com>
2024-10-01 20:47:15 -0500

    Create lucky number var and console log it

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -13,6 +13,8 @@ console.log(fullName);
 
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
+let luckyNumber = 21;
+console.log(luckyNumber);
 
 // 5 - Create a variable `introduction` and using the variables from above,
 //     give it the value of:
